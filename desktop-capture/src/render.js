9ba6370991eebc602ac053d2124d43756ee8ea1d
Baris Tikir <brstkr@icloud.com>
2020-03-25 23:19:16 +0100

    Added Project Setup & Frontend Changes(Buttons)

---
+++ desktop-capture/src/render.js
@@ -0,0 +1,19 @@
+//Modules
+
+//Buttons
+const videoElement = document.querySelector('video');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+//Trigger function on button event
+videoSelectBtn.onclick = getSources;
+
+
+//Get available video sources
+async function getSources() {
+
+
+
+
+}
