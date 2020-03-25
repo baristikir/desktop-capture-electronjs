51cb34db7843162739c28da68061e72a332a453d
Baris Tikir <brstkr@icloud.com>
2020-03-25 23:45:03 +0100

    Updated Button states

--- desktop-capture/src/render.js
+++ desktop-capture/src/render.js
@@ -9,11 +9,23 @@ const recordedChunks = [];
 
 //Buttons
 const videoElement = document.querySelector('video');
+
 const startBtn = document.getElementById('startBtn');
+startBtn.onclick = e => {
+    mediaRecorder.start();
+    startBtn.classList.add('is-danger');
+    startBtn.innerText = 'Recording';
+};
+
 const stopBtn = document.getElementById('stopBtn');
+stopBtn.onclick = e => {
+    mediaRecorder.stop();
+    startBtn.classList.remove('is-danger');
+    startBtn.innerText = 'Start';
+};
 
 const videoSelectBtn = document.getElementById('videoSelectBtn');
-//Trigger function on button event
+//Trigger function on button click event
 videoSelectBtn.onclick = getVideoSources;
 
 
