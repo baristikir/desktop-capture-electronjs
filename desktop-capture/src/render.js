a5e7d4aa990dab91c2ba52b814aa487266f77274
Baris Tikir <brstkr@icloud.com>
2020-03-25 23:29:02 +0100

    Added Modules & UI Element for Select Video Source

--- desktop-capture/src/render.js
+++ desktop-capture/src/render.js
@@ -1,5 +1,6 @@
 //Modules
-
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
 //Buttons
 const videoElement = document.querySelector('video');
 const startBtn = document.getElementById('startBtn');
@@ -7,12 +8,33 @@ const stopBtn = document.getElementById('stopBtn');
 
 const videoSelectBtn = document.getElementById('videoSelectBtn');
 //Trigger function on button event
-videoSelectBtn.onclick = getSources;
+videoSelectBtn.onclick = getVideoSources;
 
 
 //Get available video sources
-async function getSources() {
+async function getVideoSources() {
+    const inputSources = await desktopCapturer.getSources({
+        types: ['window', 'screen']
+    });
 
+    //UI Element for select source
+    const videoOptionsMenu = Menu.buildFromTemplate(
+        inputSources.map(source =>{
+            return {
+                label: source.name,
+                click: () => selectSource(source)
+            };
+        })
+    );
+
+    videoOptionsMenu.popup();
+
+    //Change the Video Source window to record
+    async function selectSource(source) {
+
+
+
+    }
 
 
 
